36f5e6beccfdb19bd99f02642c566204641235da
yamxxnx <mayanansari1991@gmail.com>
2023-07-04 11:20:14 +0530

    Initial Commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
+import "./navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="brand">
+        <Link to="/">ReactShop</Link>
+      </div>
+      <div className="links">
+        <Link to="/"> Shop </Link>
+        <Link to="/contact"> Contact </Link>
+        <Link to="/cart">
+          <ShoppingCart size={32} />
+        </Link>
+      </div>
+    </div>
+  );
+};
